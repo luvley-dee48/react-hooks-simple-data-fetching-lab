30dd50476f427fc58c3d05ab3bd3edebd87e15e3
deborah <fabbydebby@gmail.com>
2024-05-01 11:25:04 +0300

    simple-data-fetching-lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [imageUrl, setImageUrl] = useState(null);
+
+  useEffect(() => {
+    const fetchDogImage = async () => {
+      const response = await fetch('https://dog.ceo/api/breeds/image/random');
+      const data = await response.json();
+      setImageUrl(data.message);
+    };
+
+    fetchDogImage();
+  }, []);
+
+  return (
+    <div className="App">
+      {imageUrl ? (
+        <img src={imageUrl} alt="A Random Dog" />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
